c714b1e11fc39b0c67c1e66c431f8e6bb931f691
nampoe <75635660+nampoe@users.noreply.github.com>
2025-08-12 09:00:19 +0300

    Update main.php

--- meemipankki/koodi/main.php
+++ meemipankki/koodi/main.php
@@ -1,1 +1,10 @@
+<?php
+$dir = "" // laittakaa kuvien dir tänne
 
+$files = glob($dir . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+
+foreach($files as $file){
+    echo "<img src='$file' style='max-width:200px;margin:5px;'>";
+}
+
+?>
